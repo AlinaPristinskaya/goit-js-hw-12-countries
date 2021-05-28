ec446022b522c4f8adac090cfe09d8729b14e5ac
AlinaPristinskaya <pristinskayaalina9@gmail.com>
2021-05-29 00:46:30 +0300

    29.05

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './sass/main.scss';
+//import API from './fetchCountries';
+import countriesCard from "./templates/countri-card.hbs"
+
+
+const cardContainer= document.querySelector('.js-card-container');
+const search=document.querySelector(".form-control");
+
+
+var  debounce  = require('lodash.debounce') ; 
+search.addEventListener('input', onInput);
+
+
+function fetchCountries(countrieName){
+ return fetch(`https://restcountries.eu/rest/v2/name/${countrieName}`).then(
+   respons=>{
+    return respons.json();
+  });
+} 
+
+function onInput(e){
+  e.preventDefault();
+  const searchName=e.currentTarget.value;
+  console.log(searchName);
+  fetchCountries(searchName).then(renderCountriesCard);
+
+}
+
+function renderCountriesCard(countries){
+  const markup =countriesCard(countries);
+  cardContainer.innerHTML=markup;
+}
