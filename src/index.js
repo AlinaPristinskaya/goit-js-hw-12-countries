c307b43dd720115cf31cfd9a51094a7e4df304a0
AlinaPristinskaya <pristinskayaalina9@gmail.com>
2021-05-29 01:46:29 +0300

    29.05.1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
 //import API from './fetchCountries';
 import countriesCard from "./templates/countri-card.hbs"
+
+
+
+
+
+
+
 
 
 const cardContainer= document.querySelector('.js-card-container');
@@ -8,7 +16,7 @@ const search=document.querySelector(".form-control");
 
 
 var  debounce  = require('lodash.debounce') ; 
-search.addEventListener('input', onInput);
+search.addEventListener('input', debounce(onInput, 500));
 
 
 function fetchCountries(countrieName){
@@ -20,13 +28,25 @@ function fetchCountries(countrieName){
 
 function onInput(e){
   e.preventDefault();
-  const searchName=e.currentTarget.value;
+  const searchName=e.target.value;
   console.log(searchName);
   fetchCountries(searchName).then(renderCountriesCard);
+  
 
 }
 
 function renderCountriesCard(countries){
+  console.log(countries);
+  if (countries.length>10){
+    const { alert, notice, info, success, error } = require('@pnotify/core');
+
+// Manually set the type.
+
+
+const myError = error({
+  text: "необходимо сделать запрос более специфичным"
+});
+  }
   const markup =countriesCard(countries);
   cardContainer.innerHTML=markup;
 }
